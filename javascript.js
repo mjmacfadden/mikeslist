68237269d8e4ee1472c126ed757dbf680950f369
Mike Macfadden <mikemacfadden@gmail.com>
2022-12-13 11:33:42 -0600

    Dec 13

--- javascript.js
+++ javascript.js
@@ -1,16 +1,25 @@
 var array = [
+  //*** WHITE ***//
   //Hydro Flask Vacuum Insulated Stainless Steel Water Bottle Wide Mouth with Straw Lid (White, 32-Ounce)
   "<span class='edc'><a href='https://www.amazon.com/dp/B01GW2GQOI?psc=1&pd_rd_i=B01GW2GQOI&pd_rd_w=KQaA3&pf_rd_p=45a72588-80f7-4414-9851-786f6c16d42b&pd_rd_wg=8eBlP&pf_rd_r=WVQN5TDWCPV2PG5CYM0Z&pd_rd_r=adb4a7c7-a6d8-49f0-b277-441c69ef1ff0&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExWVRYQ01EQlo4UkxXJmVuY3J5cHRlZElkPUEwNTQ0NjQ1MU9ISVZOUEFTVTVERiZlbmNyeXB0ZWRBZElkPUEwNTk1NDg0MzRJUVU1MUgyWlNRViZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU%3D&linkCode=li2&tag=mikeslist-20&linkId=d7ca69a454cdd12d064d1db9a38f1c84&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01GW2GQOI&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' title='Hydro Flask'></a><br>Hydro Flask</span>",
   //YETI Rambler 20 oz Stainless Steel Vacuum Insulated Tumbler w/MagSlider Lid
   "<span class=''><a href='https://www.amazon.com/dp/B07BMDSS3L?_encoding=UTF8&psc=1&linkCode=li2&tag=mikeslist-20&linkId=86752c74ac1037038ad0ece4f3a6f425&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07BMDSS3L&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>Yeti 20 oz Tumbler</span>",
   //YETI Rambler Stainless Steel Vacuum Insulated Colster
-  "<span class=''><a href='https://www.amazon.com/dp/B07BM9DYVK?_encoding=UTF8&psc=1&linkCode=li2&tag=mikeslist-20&linkId=0fdef1db1825d5afd8950c8621f5bc48&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07BM9DYVK&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>Yeti Colster</span>",
+  "<span class='happy-hour'><a href='https://www.amazon.com/dp/B07BM9DYVK?_encoding=UTF8&psc=1&linkCode=li2&tag=mikeslist-20&linkId=0fdef1db1825d5afd8950c8621f5bc48&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07BM9DYVK&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>Yeti Colster</span>",
   //Stanley Classic Flask 8oz with Never-Lose Cap, Wide Mouth Stainless Steel Hip Flask for Easy Filling & Pouring, Insulated BPA-Free Leak-Proof Flask
-  "<span class='edc'><a href='https://www.amazon.com/dp/B07Q1DSTDJ?psc=1&pd_rd_i=B07Q1DSTDJ&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExMVJaUEVVTkJXTjNDJmVuY3J5cHRlZElkPUEwMDY4NjA1MldDS1E0TlNINkUxNyZlbmNyeXB0ZWRBZElkPUEwNTY1ODQxMjRMWkdGTEVJT0s2MCZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU%3D&linkCode=li2&tag=mikeslist-20&linkId=a4e5aa31eaa7b83a6c5f7b7c3c19aa6f&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07Q1DSTDJ&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>Stanley Flask</span>",
+  "<span class='edc happy-hour'><a href='https://www.amazon.com/dp/B07Q1DSTDJ?psc=1&pd_rd_i=B07Q1DSTDJ&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExMVJaUEVVTkJXTjNDJmVuY3J5cHRlZElkPUEwMDY4NjA1MldDS1E0TlNINkUxNyZlbmNyeXB0ZWRBZElkPUEwNTY1ODQxMjRMWkdGTEVJT0s2MCZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU%3D&linkCode=li2&tag=mikeslist-20&linkId=a4e5aa31eaa7b83a6c5f7b7c3c19aa6f&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07Q1DSTDJ&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>Stanley Flask</span>",
   //The Office Mug
-  "<span class=''><a href='https://www.amazon.com/Office-Worlds-Dunder-Mifflin-Ceramic/dp/B003GEOQDY?crid=293CQ9G14487K&keywords=world%27s%2Bbest%2Bboss%2Bmug%2Bdunder%2Bmifflin&qid=1575127432&s=office-products&sprefix=world%27s%2Bbest%2Bboss%2Bmug%2Bdunder%2Coffice-products%2C157&sr=1-3-catcorr&th=1&linkCode=li2&tag=mikeslist-20&linkId=033bc5fa9a333d663fc4ba5e615a487c&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B003GEOQDY&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>The Office Mug</span>",
+  "<span class='office'><a href='https://www.amazon.com/Office-Worlds-Dunder-Mifflin-Ceramic/dp/B003GEOQDY?crid=293CQ9G14487K&keywords=world%27s%2Bbest%2Bboss%2Bmug%2Bdunder%2Bmifflin&qid=1575127432&s=office-products&sprefix=world%27s%2Bbest%2Bboss%2Bmug%2Bdunder%2Coffice-products%2C157&sr=1-3-catcorr&th=1&linkCode=li2&tag=mikeslist-20&linkId=033bc5fa9a333d663fc4ba5e615a487c&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B003GEOQDY&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>The Office Mug</span>",
+  //SMART PLUG
+  "<span class='electronics'><a href='https://www.amazon.com/TP-Link-Kasa-Smart-Wifi-Plug/dp/B07RCNB2L3?crid=3KLN3AZATSQ6L&keywords=smart+plug&qid=1670952038&sprefix=smart+plu%2Caps%2C130&sr=8-6&linkCode=li2&tag=mikeslist-20&linkId=87b7d02dde4308d178e666b625f42420&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07RCNB2L3&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>Smart Plug</span>",
+  //*** BLACK  ***/
+  //Smartish iPhone X/XS Wallet Case
+  "<span class='misc'><a href='https://www.amazon.com/Silk-iPhone-Wallet-Case-Protective/dp/B073V75W3M?crid=2086MS8AMDHZ3&keywords=silk+iphone+xs+case+wallet&qid=1575129432&smid=ATVPDKIKX0DER&sprefix=silk+iphone+xs+case+%2Caps%2C186&sr=8-4&linkCode=li2&tag=mikeslist-20&linkId=08f1d003a51a165a0a45c723aac46455&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B073V75W3M&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>Wallet Phone Case></span>",
+  //AVANTEK Car Mount Universal Air Vent Cradle, Universal Phone Holder
+  "<span class='misc'><a href='https://www.amazon.com/gp/product/B00ZLPJ10Y?ie=UTF8&psc=1&linkCode=li2&tag=mikeslist-20&linkId=3dc27feb2fe210ec51174725fab7ee33&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00ZLPJ10Y&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>Cell Phone Holder</span>",
+  //*** BROWN ***//
   //Killerspin JET200 Ping Pong Paddle
-  "<span class=''><a href='https://www.amazon.com/dp/B01LZEWZGE?_encoding=UTF8&th=1&linkCode=li2&tag=mikeslist-20&linkId=496bb45e2403f659cb16b5599d538beb&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01LZEWZGE&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>Ping Pong Paddle</span>",
+  "<span class='sport misc'><a href='https://www.amazon.com/dp/B01LZEWZGE?_encoding=UTF8&th=1&linkCode=li2&tag=mikeslist-20&linkId=496bb45e2403f659cb16b5599d538beb&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01LZEWZGE&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><br>Ping Pong Paddle</span>",
 ];
 
 // Get the element with the id "array-values"
@@ -20,33 +29,53 @@ var arrayValuesElement = document.getElementById("array-values");
 for (var i = 0; i < array.length; i++) {
   // Create a new list item for each array value
   var newListItem = document.createElement("div");
-  newListItem.className = "col-6 col-sm-3 col-md-2 py-3 product";
+  newListItem.className = "col-6 col-sm-4 col-md-3 col-lg-2 py-3 product";
   newListItem.innerHTML = array[i];
 
   // Append the new list item to the array values element
   arrayValuesElement.appendChild(newListItem);
 }
 
-//Filter Buttons
-function showAll() {
-  // select all elements with the "product" class
-  const products = document.querySelectorAll(".product");
+//FILTERS
 
-  // iterate over the selected elements and set their display property to none
-  for (let i = 0; i < products.length; i++) {
-    products[i].style.opacity = "1";
-    products[i].style.display = "inline-block";
-  }
-}
+// select all elements with the "product" class
+const products = document.querySelectorAll(".product");
 
-function edcFilter() {
-  // select all elements with the "product" class
-  const products = document.querySelectorAll(".product");
+function hideAll() {
   // iterate over the selected elements and set their display property to none
   for (let i = 0; i < products.length; i++) {
     products[i].style.display = "none";
   }
+}
+function showAll() {
+  // iterate over the selected elements and set their display property to in-line
+  for (let i = 0; i < products.length; i++) {
+    products[i].style.display = "inline-block";
+  }
+}
+function edcFilter() {
+  hideAll();
   $("span.edc").parent().css("display", "block");
+}
+function electronicsFilter() {
+  hideAll();
+  $("span.electronics").parent().css("display", "block");
+}
+function happyHourFilter() {
+  hideAll();
+  $("span.happy-hour").parent().css("display", "block");
+}
+function officeFilter() {
+  hideAll();
+  $("span.office").parent().css("display", "block");
+}
+function miscFilter() {
+  hideAll();
+  $("span.misc").parent().css("display", "block");
+}
+function sportFilter() {
+  hideAll();
+  $("span.sport").parent().css("display", "block");
 }
 
 //SETTINGS PAGE
