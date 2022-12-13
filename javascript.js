5bd3e2da90140c7f0ceb3bdf9e2ca908ba21524a
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2022-12-13 09:30:28 -0600

    Dec 13

--- javascript.js
+++ javascript.js
@@ -1,15 +1,16 @@
 var array = [
   //Hydro Flask Vacuum Insulated Stainless Steel Water Bottle Wide Mouth with Straw Lid (White, 32-Ounce)
-  "<a href='https://www.amazon.com/dp/B01GW2GQOI?psc=1&pd_rd_i=B01GW2GQOI&pd_rd_w=KQaA3&pf_rd_p=45a72588-80f7-4414-9851-786f6c16d42b&pd_rd_wg=8eBlP&pf_rd_r=WVQN5TDWCPV2PG5CYM0Z&pd_rd_r=adb4a7c7-a6d8-49f0-b277-441c69ef1ff0&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExWVRYQ01EQlo4UkxXJmVuY3J5cHRlZElkPUEwNTQ0NjQ1MU9ISVZOUEFTVTVERiZlbmNyeXB0ZWRBZElkPUEwNTk1NDg0MzRJUVU1MUgyWlNRViZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU%3D&linkCode=li2&tag=mikeslist-20&linkId=d7ca69a454cdd12d064d1db9a38f1c84&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01GW2GQOI&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' title='Hydro Flask'></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B01GW2GQOI' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>Hydro Flask",
+  "<span class='edc'><a href='https://www.amazon.com/dp/B01GW2GQOI?psc=1&pd_rd_i=B01GW2GQOI&pd_rd_w=KQaA3&pf_rd_p=45a72588-80f7-4414-9851-786f6c16d42b&pd_rd_wg=8eBlP&pf_rd_r=WVQN5TDWCPV2PG5CYM0Z&pd_rd_r=adb4a7c7-a6d8-49f0-b277-441c69ef1ff0&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExWVRYQ01EQlo4UkxXJmVuY3J5cHRlZElkPUEwNTQ0NjQ1MU9ISVZOUEFTVTVERiZlbmNyeXB0ZWRBZElkPUEwNTk1NDg0MzRJUVU1MUgyWlNRViZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU%3D&linkCode=li2&tag=mikeslist-20&linkId=d7ca69a454cdd12d064d1db9a38f1c84&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01GW2GQOI&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' title='Hydro Flask'></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B01GW2GQOI' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>Hydro Flask</span>",
   //YETI Rambler 20 oz Stainless Steel Vacuum Insulated Tumbler w/MagSlider Lid
-  "<a href='https://www.amazon.com/dp/B07BMDSS3L?_encoding=UTF8&psc=1&linkCode=li2&tag=mikeslist-20&linkId=86752c74ac1037038ad0ece4f3a6f425&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07BMDSS3L&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B07BMDSS3L' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>Yeti 20 oz Tumbler",
+  "<span class=''><a href='https://www.amazon.com/dp/B07BMDSS3L?_encoding=UTF8&psc=1&linkCode=li2&tag=mikeslist-20&linkId=86752c74ac1037038ad0ece4f3a6f425&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07BMDSS3L&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B07BMDSS3L' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>Yeti 20 oz Tumbler</span>",
   //YETI Rambler Stainless Steel Vacuum Insulated Colster
-  "<a href='https://www.amazon.com/dp/B07BM9DYVK?_encoding=UTF8&psc=1&linkCode=li2&tag=mikeslist-20&linkId=0fdef1db1825d5afd8950c8621f5bc48&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07BM9DYVK&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B07BM9DYVK' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>Yeti Colster",
+  "<span class=''><a href='https://www.amazon.com/dp/B07BM9DYVK?_encoding=UTF8&psc=1&linkCode=li2&tag=mikeslist-20&linkId=0fdef1db1825d5afd8950c8621f5bc48&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07BM9DYVK&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B07BM9DYVK' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>Yeti Colster</span>",
   //Stanley Classic Flask 8oz with Never-Lose Cap, Wide Mouth Stainless Steel Hip Flask for Easy Filling & Pouring, Insulated BPA-Free Leak-Proof Flask
-  "<a href='https://www.amazon.com/dp/B07Q1DSTDJ?psc=1&pd_rd_i=B07Q1DSTDJ&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExMVJaUEVVTkJXTjNDJmVuY3J5cHRlZElkPUEwMDY4NjA1MldDS1E0TlNINkUxNyZlbmNyeXB0ZWRBZElkPUEwNTY1ODQxMjRMWkdGTEVJT0s2MCZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU%3D&linkCode=li2&tag=mikeslist-20&linkId=a4e5aa31eaa7b83a6c5f7b7c3c19aa6f&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07Q1DSTDJ&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B07Q1DSTDJ' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>Stanley Flask",
-  "<a href='https://www.amazon.com/Office-Worlds-Dunder-Mifflin-Ceramic/dp/B003GEOQDY?crid=293CQ9G14487K&keywords=world%27s%2Bbest%2Bboss%2Bmug%2Bdunder%2Bmifflin&qid=1575127432&s=office-products&sprefix=world%27s%2Bbest%2Bboss%2Bmug%2Bdunder%2Coffice-products%2C157&sr=1-3-catcorr&th=1&linkCode=li2&tag=mikeslist-20&linkId=033bc5fa9a333d663fc4ba5e615a487c&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B003GEOQDY&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B003GEOQDY' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>The Office Mug",
+  "<span class='edc'><a href='https://www.amazon.com/dp/B07Q1DSTDJ?psc=1&pd_rd_i=B07Q1DSTDJ&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExMVJaUEVVTkJXTjNDJmVuY3J5cHRlZElkPUEwMDY4NjA1MldDS1E0TlNINkUxNyZlbmNyeXB0ZWRBZElkPUEwNTY1ODQxMjRMWkdGTEVJT0s2MCZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU%3D&linkCode=li2&tag=mikeslist-20&linkId=a4e5aa31eaa7b83a6c5f7b7c3c19aa6f&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07Q1DSTDJ&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B07Q1DSTDJ' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>Stanley Flask</span>",
+  //The Office Mug
+  "<span class=''><a href='https://www.amazon.com/Office-Worlds-Dunder-Mifflin-Ceramic/dp/B003GEOQDY?crid=293CQ9G14487K&keywords=world%27s%2Bbest%2Bboss%2Bmug%2Bdunder%2Bmifflin&qid=1575127432&s=office-products&sprefix=world%27s%2Bbest%2Bboss%2Bmug%2Bdunder%2Coffice-products%2C157&sr=1-3-catcorr&th=1&linkCode=li2&tag=mikeslist-20&linkId=033bc5fa9a333d663fc4ba5e615a487c&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B003GEOQDY&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B003GEOQDY' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>The Office Mug</span>",
   //Killerspin JET200 Ping Pong Paddle
-  "<a href='https://www.amazon.com/dp/B01LZEWZGE?_encoding=UTF8&th=1&linkCode=li2&tag=mikeslist-20&linkId=496bb45e2403f659cb16b5599d538beb&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01LZEWZGE&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B01LZEWZGE' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>Ping Pong Paddle",
+  "<span class=''><a href='https://www.amazon.com/dp/B01LZEWZGE?_encoding=UTF8&th=1&linkCode=li2&tag=mikeslist-20&linkId=496bb45e2403f659cb16b5599d538beb&language=en_US&ref_=as_li_ss_il' target='_blank'><img border='0' src='//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B01LZEWZGE&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=mikeslist-20&language=en_US' ></a><img src='https://ir-na.amazon-adsystem.com/e/ir?t=mikeslist-20&language=en_US&l=li2&o=1&a=B01LZEWZGE' width='1' height='1' border='0' alt='' style='border:none !important; margin:0px !important;' /><br>Ping Pong Paddle</span>",
 ];
 
 // Get the element with the id "array-values"
@@ -19,9 +20,40 @@ var arrayValuesElement = document.getElementById("array-values");
 for (var i = 0; i < array.length; i++) {
   // Create a new list item for each array value
   var newListItem = document.createElement("div");
-  newListItem.className = "col-6 col-sm-3 col-md-2 py-3";
+  newListItem.className = "col-6 col-sm-3 col-md-2 py-3 product";
   newListItem.innerHTML = array[i];
 
   // Append the new list item to the array values element
   arrayValuesElement.appendChild(newListItem);
 }
+
+//Filter Buttons
+function showAll() {
+  // select all elements with the "product" class
+  const products = document.querySelectorAll('.product');
+
+  // iterate over the selected elements and set their display property to none
+  for (let i = 0; i < products.length; i++) {
+    products[i].style.opacity = '1';
+    products[i].style.display = 'inline-block';
+  }
+};
+
+
+function edcFilter() {
+  // select all elements with the "product" class
+  const products = document.querySelectorAll('.product');
+
+  // iterate over the selected elements and set their display property to none
+  for (let i = 0; i < products.length; i++) {
+    products[i].style.opacity = '0';
+    products[i].style.display = 'none';
+  }
+  const edc = document.querySelectorAll('.edc');
+
+  // iterate over the selected elements and set their display property to none
+  for (let i = 0; i < edc.length; i++) {
+    edc[i].style.opacity = '1';
+    edc[i].style.display = 'inline-block';
+  }
+};
